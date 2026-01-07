968c5945da10f8cd680751612968b8adc92354fc
NarendraLanka <narendranadh.lanka@gmail.com>
2026-01-07 10:26:10 +0530

    Create main.tf

---
+++ modules/s3/main.tf
@@ -0,0 +1,29 @@
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  tags   = merge(var.tags, { Name = var.bucket_name })
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}

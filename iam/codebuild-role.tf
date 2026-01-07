1b8f9c0814a739c69ff91f1530aedd4df52d7f93
NarendraLanka <narendranadh.lanka@gmail.com>
2026-01-07 10:36:32 +0530

    Create codebuild-role.tf

---
+++ iam/codebuild-role.tf
@@ -0,0 +1,21 @@
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codebuild" {
+  name               = "${var.project}-${var.environment}-codebuild-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags               = { Project = var.project, Environment = var.environment, ManagedBy = "Terraform" }
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
